c8b25d5e419137b95f037892c0321fa6f2fdbd79
Priscila Fernandes de Lima <prisccrap@gmail.com>
2025-10-14 04:00:40 -0300

    Add files via upload

--- teste basico em visual studio.py
+++ teste basico em visual studio.py
@@ -1,1 +1,34 @@
+a = 0
+for i in range(30):
+    if a % 2 == 0:
+        a += 1
+        continue
+    else:
+        if a % 5 == 0:
+            break
+        else:
+            a += 3
+print (a)
 
+def soma(a ,b):
+    return a + b
+resultado = soma(3, 5)
+
+print("A Soma é", resultado)
+
+
+#Variavéis#
+idade = 25
+nome = "João"
+altura = 1.75
+#Exibir dados
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+#Manipular de dados
+idade = 26
+#Controle de Fluxo, usando estrutura condicional if e else
+if idade >= 18:
+    print(nome, "é maior de idade.")
+else:
+    print(nome, "é menor de idade")
